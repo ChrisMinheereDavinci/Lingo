e328e4793cf20f143c21e3aee57c80fb53aac160
Chris_Minheere <99040522@mydavinci.nl>
2018-12-16 21:36:44 +0100

    Lingo + lab

---
+++ Lab 1/js/main.js
@@ -0,0 +1,11 @@
+function backgroundColorGreen(){
+    document.body.style.backgroundColor = "green";
+}
+
+function backgroundColorRed(){
+    document.body.style.backgroundColor = "Red";
+}
+
+function backgroundColorBlue(){
+    document.body.style.backgroundColor = "Blue";
+}
